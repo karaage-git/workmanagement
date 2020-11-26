0d58f9a0b43cc5ac67733f48a3514b2c7496b6f7
karaage-git <74481735+karaage-git@users.noreply.github.com>
2020-11-26 22:16:53 +0900

    ・ツールバーのレイアウト、アイコンを追加 ・トップメニューのモックを作成 ・その他リソースを追加

--- WorkManagement/app/src/main/java/com/karaageumai/workmanagement/view/TopMenuActivity.kt
+++ WorkManagement/app/src/main/java/com/karaageumai/workmanagement/view/TopMenuActivity.kt
@@ -2,6 +2,9 @@ package com.karaageumai.workmanagement.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.appcompat.widget.Toolbar
 import com.karaageumai.workmanagement.R
 import com.karaageumai.workmanagement.util.Log
 
@@ -10,7 +13,18 @@ class TopMenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_menu)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        toolbar.title = getString(R.string.top_menu)
+        setSupportActionBar(toolbar)
 
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_empty, menu)
+        return true
+    }
+
+
 }
