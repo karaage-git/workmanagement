a2c1580691f1dddb1f0f6a4c171ded4495cebd1e
karaage-git <74481735+karaage-git@users.noreply.github.com>
2020-12-28 21:30:07 +0900

    [ BonusInfo, SalaryInfo ]インデントを見直し

--- WorkManagement/app/src/main/java/com/karaageumai/workmanagement/model/bonus/BonusInfo.kt
+++ WorkManagement/app/src/main/java/com/karaageumai/workmanagement/model/bonus/BonusInfo.kt
@@ -7,28 +7,28 @@ import java.io.Serializable
 
 @Entity(tableName = "bonus_table")
 data class BonusInfo(
-    // 主キー
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    // 支給年
-    val year: Int,
-    // 支給月
-    val month: Int,
-    // 支給額
-    val bonus: Int,
-    // その他の支給
-    val otherPayment: Int,
-    // 健康保険料
-    @ColumnInfo(name = "health_insurance_fee") var healthInsuranceFee: Int = 0,
-    // 介護保険料
-    @ColumnInfo(name = "long_term_care_insurance_fee") var longTermCareInsuranceFee: Int = 0,
-    // 年金保険料
-    @ColumnInfo(name = "pension_fee") var pensionFee: Int = 0,
-    // 雇用保険料
-    @ColumnInfo(name = "employment_insurance_fee") var employmentInsuranceFee: Int = 0,
-    // 所得税
-    @ColumnInfo(name = "income_tax") var incomeTax: Int = 0,
-    // その他
-    var other: Int = 0,
-    // 完了フラグ
-    @ColumnInfo(name = "is_complete") var isComplete: Boolean = false
-    ) : Serializable
+        // 主キー
+        @PrimaryKey(autoGenerate = true) val id: Int,
+        // 支給年
+        val year: Int,
+        // 支給月
+        val month: Int,
+        // 支給額
+        val bonus: Int,
+        // その他の支給
+        val otherPayment: Int,
+        // 健康保険料
+        @ColumnInfo(name = "health_insurance_fee") var healthInsuranceFee: Int = 0,
+        // 介護保険料
+        @ColumnInfo(name = "long_term_care_insurance_fee") var longTermCareInsuranceFee: Int = 0,
+        // 年金保険料
+        @ColumnInfo(name = "pension_fee") var pensionFee: Int = 0,
+        // 雇用保険料
+        @ColumnInfo(name = "employment_insurance_fee") var employmentInsuranceFee: Int = 0,
+        // 所得税
+        @ColumnInfo(name = "income_tax") var incomeTax: Int = 0,
+        // その他
+        var other: Int = 0,
+        // 完了フラグ
+        @ColumnInfo(name = "is_complete") var isComplete: Boolean = false
+) : Serializable
--- WorkManagement/app/src/main/java/com/karaageumai/workmanagement/model/salary/SalaryInfo.kt
+++ WorkManagement/app/src/main/java/com/karaageumai/workmanagement/model/salary/SalaryInfo.kt
@@ -7,40 +7,40 @@ import java.io.Serializable
 
 @Entity(tableName = "salary_table")
 data class SalaryInfo(
-    // 主キー
-    @PrimaryKey(autoGenerate = true) val id: Int,
-    // ターゲットとなる年
-    val year: Int,
-    // ターゲットとなる月
-    val month: Int,
-    // 勤務日数（半休を考慮）
-    @ColumnInfo(name = "working_day") var workingDay: Double = 0.0,
-    // 勤務時間（1時間未満を考慮）
-    @ColumnInfo(name = "working_time") var workingTime: Double = 0.0,
-    // 残業時間（1時間未満を考慮）
-    var overtime: Double = 0.0,
-    // 有給休暇日数
-    @ColumnInfo(name = "paid_holidays") var paidHolidays: Double = 0.0,
-    // 給料
-    var baseIncome: Int = 0,
-    // 残業手当
-    @ColumnInfo(name = "overtime_salary") var overtimeIncome: Int = 0,
-    // その他（資格報奨金など）
-    @ColumnInfo(name = "other_income") var otherIncome: Int = 0,
-    // 健康保険料
-    @ColumnInfo(name = "health_insurance_fee") var healthInsuranceFee: Int = 0,
-    // 介護保険料
-    @ColumnInfo(name = "longterm_care_insurance_fee") var longtermCareInsuranceFee: Int = 0,
-    // 年金保険料
-    @ColumnInfo(name = "pension_fee") var pensionFee: Int = 0,
-    // 雇用保険料
-    @ColumnInfo(name = "employment_insurance_fee") var employmentInsuranceFee: Int = 0,
-    // 所得税
-    @ColumnInfo(name = "income_tax") var incomeTax: Int = 0,
-    // 住民税
-    @ColumnInfo(name = "resident_tax") var residentTax: Int = 0,
-    // その他
-    var other: Int = 0,
-    // 完了フラグ
-    @ColumnInfo(name = "is_complete") var isComplete: Boolean = false
+        // 主キー
+        @PrimaryKey(autoGenerate = true) val id: Int,
+        // ターゲットとなる年
+        val year: Int,
+        // ターゲットとなる月
+        val month: Int,
+        // 勤務日数（半休を考慮）
+        @ColumnInfo(name = "working_day") var workingDay: Double = 0.0,
+        // 勤務時間（1時間未満を考慮）
+        @ColumnInfo(name = "working_time") var workingTime: Double = 0.0,
+        // 残業時間（1時間未満を考慮）
+        var overtime: Double = 0.0,
+        // 有給休暇日数
+        @ColumnInfo(name = "paid_holidays") var paidHolidays: Double = 0.0,
+        // 給料
+        var baseIncome: Int = 0,
+        // 残業手当
+        @ColumnInfo(name = "overtime_salary") var overtimeIncome: Int = 0,
+        // その他（資格報奨金など）
+        @ColumnInfo(name = "other_income") var otherIncome: Int = 0,
+        // 健康保険料
+        @ColumnInfo(name = "health_insurance_fee") var healthInsuranceFee: Int = 0,
+        // 介護保険料
+        @ColumnInfo(name = "longterm_care_insurance_fee") var longtermCareInsuranceFee: Int = 0,
+        // 年金保険料
+        @ColumnInfo(name = "pension_fee") var pensionFee: Int = 0,
+        // 雇用保険料
+        @ColumnInfo(name = "employment_insurance_fee") var employmentInsuranceFee: Int = 0,
+        // 所得税
+        @ColumnInfo(name = "income_tax") var incomeTax: Int = 0,
+        // 住民税
+        @ColumnInfo(name = "resident_tax") var residentTax: Int = 0,
+        // その他
+        var other: Int = 0,
+        // 完了フラグ
+        @ColumnInfo(name = "is_complete") var isComplete: Boolean = false
 ) : Serializable
