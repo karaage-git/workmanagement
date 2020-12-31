eae0b9c3c623f2b7521166a71ad93b5ccafe2c2b
karaage-git <74481735+karaage-git@users.noreply.github.com>
2020-12-31 19:54:52 +0900

    [ NumberFormatUtil ]クラス構成を見直し

--- WorkManagement/app/src/main/java/com/karaageumai/workmanagement/util/NumberFormatUtil.kt
+++ WorkManagement/app/src/main/java/com/karaageumai/workmanagement/util/NumberFormatUtil.kt
@@ -2,104 +2,97 @@ package com.karaageumai.workmanagement.util
 
 import java.lang.NumberFormatException
 
-class NumberFormatUtil {
-
-    companion object {
-
-        /**
-         * 文字列が整数として解釈できるかチェックする
-         *
-         * @param aSting 検査対象文字列
-         * @return true:整数 , false:整数以外
-         */
-        fun checkNaturalNumberFormat(aSting: String): Boolean {
-            // まず、数値にキャストできるか確かめる
-            try {
-                aSting.toInt()
-            } catch (e: NumberFormatException) {
-                return false
-            }
-
-            // 最初の小数点の位置を調べる
-            return when (aSting.indexOf(".")) {
-                // 整数の場合
-                -1 -> true
-
-                // 一番最後が小数点の場合は、整数とみなす
-                (aSting.length - 1) -> true
-
-                // それ以外はfalse
-                else -> false
-            }
+object NumberFormatUtil {
+    /**
+     * 文字列が整数として解釈できるかチェックする
+     *
+     * @param aSting 検査対象文字列
+     * @return true:整数 , false:整数以外
+     */
+    fun checkNaturalNumberFormat(aSting: String): Boolean {
+        // まず、数値にキャストできるか確かめる
+        try {
+            aSting.toInt()
+        } catch (e: NumberFormatException) {
+            return false
         }
 
-        /**
-         * 文字列が0.1単位の数値として解釈できるかチェックする
-         *
-         * @param aSting 検査対象文字列
-         * @return true:0.1単位 , false:0.1単位以外
-         */
-        fun checkNumberFormat01(aSting: String): Boolean {
-            // まず、数値にキャストできるか確かめる
-            try {
-                aSting.toDouble()
-            } catch (e: NumberFormatException) {
-                return false
-            }
+        // 最初の小数点の位置を調べる
+        return when (aSting.indexOf(".")) {
+            // 整数の場合
+            -1 -> true
 
-            // 最初の小数点の位置を調べる
-            return when (aSting.indexOf(".")) {
-                // 整数の場合
-                -1 -> true
+            // 一番最後が小数点の場合は、整数とみなす
+            (aSting.length - 1) -> true
 
-                // 一番最後が小数点の場合は、整数とみなす
-                (aSting.lastIndex) -> true
-
-                // 小数点以下が1桁しか無い場合
-                (aSting.lastIndex - 1) -> true
-
-                // それ以外はfalse
-                else -> false
-            }
+            // それ以外はfalse
+            else -> false
         }
-
-        /**
-         * 文字列が0.5単位の数値として解釈できるかチェックする
-         *
-         * @param aSting 検査対象文字列
-         * @return true:0.5単位 , false:0.5単位以外
-         */
-        fun checkNumberFormat05(aSting: String): Boolean {
-            // まず、数値にキャストできるか確かめる
-            try {
-                aSting.toDouble()
-            } catch (e: NumberFormatException) {
-                return false
-            }
-
-            // 最初の小数点の位置を調べる
-            return when (aSting.indexOf(".")) {
-                // 整数の場合
-                -1 -> true
-
-                // 一番最後が小数点の場合は、整数とみなす
-                (aSting.lastIndex) -> true
-
-                // 小数点以下が1桁が0 or 5の場合はOK
-                (aSting.lastIndex - 1) -> {
-                    when (aSting.last()) {
-                        '0' -> true
-                        '5' -> true
-                        else -> false
-                    }
-                }
-
-                // それ以外はfalse
-                else -> false
-            }
-        }
-
-
     }
 
+    /**
+     * 文字列が0.1単位の数値として解釈できるかチェックする
+     *
+     * @param aSting 検査対象文字列
+     * @return true:0.1単位 , false:0.1単位以外
+     */
+    fun checkNumberFormat01(aSting: String): Boolean {
+        // まず、数値にキャストできるか確かめる
+        try {
+            aSting.toDouble()
+        } catch (e: NumberFormatException) {
+            return false
+        }
+
+        // 最初の小数点の位置を調べる
+        return when (aSting.indexOf(".")) {
+            // 整数の場合
+            -1 -> true
+
+            // 一番最後が小数点の場合は、整数とみなす
+            (aSting.lastIndex) -> true
+
+            // 小数点以下が1桁しか無い場合
+            (aSting.lastIndex - 1) -> true
+
+            // それ以外はfalse
+            else -> false
+        }
+    }
+
+    /**
+     * 文字列が0.5単位の数値として解釈できるかチェックする
+     *
+     * @param aSting 検査対象文字列
+     * @return true:0.5単位 , false:0.5単位以外
+     */
+    fun checkNumberFormat05(aSting: String): Boolean {
+        // まず、数値にキャストできるか確かめる
+        try {
+            aSting.toDouble()
+        } catch (e: NumberFormatException) {
+            return false
+        }
+
+        // 最初の小数点の位置を調べる
+        return when (aSting.indexOf(".")) {
+            // 整数の場合
+            -1 -> true
+
+            // 一番最後が小数点の場合は、整数とみなす
+            (aSting.lastIndex) -> true
+
+            // 小数点以下が1桁が0 or 5の場合はOK
+            (aSting.lastIndex - 1) -> {
+                when (aSting.last()) {
+                    '0' -> true
+                    '5' -> true
+                    else -> false
+                }
+            }
+
+            // それ以外はfalse
+            else -> false
+        }
+    }
 }
