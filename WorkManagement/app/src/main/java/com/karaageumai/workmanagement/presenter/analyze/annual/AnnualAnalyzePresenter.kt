b6519c6e3bf5d84749820c9c091299d7c127b10c
karaage-git <74481735+karaage-git@users.noreply.github.com>
2021-01-11 23:12:41 +0900

    [ AnnualAnalyzePresenter ]各種合計値を算出する実装を追加

--- WorkManagement/app/src/main/java/com/karaageumai/workmanagement/presenter/analyze/annual/AnnualAnalyzePresenter.kt
+++ WorkManagement/app/src/main/java/com/karaageumai/workmanagement/presenter/analyze/annual/AnnualAnalyzePresenter.kt
@@ -30,30 +30,223 @@ class AnnualAnalyzePresenter(var mActivity: IAnnualAnalyze) : IAnnualAnalyzePres
 
     override fun loadData(aYear: Int) {
         val dataRowList: MutableList<AnnualDataRow> = mutableListOf()
-        dataRowList.add(getSumWorkingDayDataRow(aYear))
-        dataRowList.add(getSumWorkingTimeDataRow(aYear))
-        dataRowList.add(getSumIncomeDataRow(aYear))
-        dataRowList.add(getSumBaseIncome(aYear))
+        dataRowList.add(createSumWorkingDayDataRow(aYear))
+        dataRowList.add(createSumWorkingTimeDataRow(aYear))
+        dataRowList.add(createSumBaseWorkingTimeDataRow(aYear))
+        dataRowList.add(createSumOverWorkingTimeDataRow(aYear))
+        dataRowList.add(createSumIncomeDataRow(aYear))
+        dataRowList.add(createSumBaseIncomeDataRow(aYear))
+        dataRowList.add(createSumOverTimeIncomeDataRow(aYear))
+        dataRowList.add(createSumOtherIncomeDataRow(aYear))
+        dataRowList.add(createSumBonusDataRow(aYear))
+        dataRowList.add(createSumDeductionDataRow(aYear))
+        dataRowList.add(createSumHealthInsuranceFeeDataRow(aYear))
+        dataRowList.add(createSumLongTermCareInsuranceFeeDataRow(aYear))
+        dataRowList.add(createSumPensionInsuranceFeeDataRow(aYear))
+        dataRowList.add(createSumEmploymentInsuranceFeeDataRow(aYear))
+        dataRowList.add(createSumIncomeTaxDataRow(aYear))
+        dataRowList.add(createSumResidentTaxDataRow(aYear))
+        dataRowList.add(createSumOtherDeductionDataRow(aYear))
+        dataRowList.add(createSumAfterTaxIncomeDataRow(aYear))
         mActivity.onLoadedData(dataRowList)
     }
 
     /**
+     * 勤務日数の合計を表示するためのデータを作成する
+     */
+    private fun createSumWorkingDayDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_working_day,
+                (getSumWorkingDay(aYear) / 10.0).toString(),
+                R.string.annual_analyze_row_unit_day,
+                false)
+    }
+
+    /**
+     * 勤務時間の合計を表示するためのデータを作成する
+     */
+    private fun createSumWorkingTimeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_working_time,
+                (getSumWorkingTime(aYear) / 10.0).toString(),
+                R.string.annual_analyze_row_unit_time,
+                false)
+    }
+
+    /**
+     * 所定労働時間の合計を表示するためのデータを作成する
+     */
+    private fun createSumBaseWorkingTimeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_base_working_time,
+                (getSumBaseWorkingTime(aYear) / 10.0).toString(), R.string.annual_analyze_row_unit_time, true)
+    }
+
+    /**
+     * 残業時間の合計を表示するためのデータを作成する
+     */
+    private fun createSumOverWorkingTimeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_over_working_time,
+                (getSumOverWorkingTime(aYear) / 10.0).toString(), R.string.annual_analyze_row_unit_time,
+                true)
+    }
+
+    /**
+     * 収入の合計を表示するためのデータを作成する
+     */
+    private fun createSumIncomeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_income,
+                getSumIncome(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                false)
+    }
+
+    /**
+     * 基本給の合計を表示するためのデータを作成する
+     */
+    private fun createSumBaseIncomeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_base_income,
+                getSumBaseIncome(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 残業手当の合計を表示するためのデータを作成する
+     */
+    private fun createSumOverTimeIncomeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_overtime_income,
+                getSumOverTimeIncome(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * その他収入の合計を表示するためのデータを作成する
+     */
+    private fun createSumOtherIncomeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_other_income,
+                getSumOtherIncome(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * ボーナスの合計を表示するためのデータを作成する
+     */
+    private fun createSumBonusDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_bonus,
+                getSumBonus(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 控除の合計を表示するためのデータを作成する
+     */
+    private fun createSumDeductionDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction,
+                getSumDeduction(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                false)
+    }
+
+    /**
+     * 健康保険の合計を表示するためのデータを作成する
+     */
+    private fun createSumHealthInsuranceFeeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction_health,
+                getSumHealthInsuranceFee(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 介護保険の合計を表示するためのデータを作成する
+     */
+    private fun createSumLongTermCareInsuranceFeeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction_long_term,
+                getSumLongTermCareInsuranceFee(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 年金保険の合計を表示するためのデータを作成する
+     */
+    private fun createSumPensionInsuranceFeeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction_pension,
+                getSumPensionFee(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 雇用保険の合計を表示するためのデータを作成する
+     */
+    private fun createSumEmploymentInsuranceFeeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction_employment,
+                getSumEmploymentInsuranceFee(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 所得税の合計を表示するためのデータを作成する
+     */
+    private fun createSumIncomeTaxDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction_income,
+                getSumIncomeTax(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 住民税の合計を表示するためのデータを作成する
+     */
+    private fun createSumResidentTaxDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction_resident,
+                getSumResidentTax(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * その他控除の合計を表示するためのデータを作成する
+     */
+    private fun createSumOtherDeductionDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_deduction_other,
+                getSumOtherDeduction(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                true)
+    }
+
+    /**
+     * 手取りの合計を表示するためのデータを作成する
+     */
+    private fun createSumAfterTaxIncomeDataRow(aYear: Int): AnnualDataRow {
+        return AnnualDataRow(R.string.annual_analyze_row_after_tax_income,
+                getSumAfterTaxIncome(aYear).toString(),
+                R.string.annual_analyze_row_unit_money,
+                false)
+    }
+
+
+
+    /**
      * 勤務日数の合計を算出する
      */
-    private fun getSumWorkingDayDataRow(aYear: Int): AnnualDataRow {
+    private fun getSumWorkingDay(aYear: Int): Int {
         val data = getAnnualData(aYear)
         val salaryInfoList = data.salaryInfoList
         var value = 0
         for(salary in salaryInfoList) {
             value += salary.workingDay
         }
-        return AnnualDataRow(R.string.annual_analyze_row_working_day, (value / 10.0).toString(), R.string.annual_analyze_row_unit_day, false)
+        return value
     }
 
     /**
      * 勤務時間の合計を算出する
      */
-    private fun getSumWorkingTimeDataRow(aYear: Int): AnnualDataRow {
+    private fun getSumWorkingTime(aYear: Int): Int {
         val data = getAnnualData(aYear)
         val salaryInfoList = data.salaryInfoList
         var value = 0
@@ -63,13 +256,41 @@ class AnnualAnalyzePresenter(var mActivity: IAnnualAnalyze) : IAnnualAnalyzePres
             // 残業時間を加算
             value += salary.overtime
         }
-        return AnnualDataRow(R.string.annual_analyze_row_working_time, (value / 10.0).toString(), R.string.annual_analyze_row_unit_time, false)
+        return value
+    }
+
+    /**
+     * 所定労働時間の合計を算出する
+     */
+    private fun getSumBaseWorkingTime(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        var value = 0
+        for(salary in salaryInfoList) {
+            // 通常の労働時間を加算
+            value += salary.workingTime
+        }
+        return value
+    }
+
+    /**
+     * 残業時間の合計を算出する
+     */
+    private fun getSumOverWorkingTime(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        var value = 0
+        for(salary in salaryInfoList) {
+            // 通常の残業時間を加算
+            value += salary.overtime
+        }
+        return value
     }
 
     /**
      * 収入の合計を算出する
      */
-    private fun getSumIncomeDataRow(aYear: Int): AnnualDataRow {
+    private fun getSumIncome(aYear: Int): Int {
         val data = getAnnualData(aYear)
         val salaryInfoList = data.salaryInfoList
         val bonusInfoList = data.bonusInfoList
@@ -88,13 +309,13 @@ class AnnualAnalyzePresenter(var mActivity: IAnnualAnalyze) : IAnnualAnalyzePres
             // その他
             value += bonusInfo.otherIncome
         }
-        return AnnualDataRow(R.string.annual_analyze_row_income, value.toString(), R.string.annual_analyze_row_unit_money, false)
+        return value
     }
 
     /**
      * 基本給の合計を算出する
      */
-    private fun getSumBaseIncome(aYear: Int): AnnualDataRow {
+    private fun getSumBaseIncome(aYear: Int): Int {
         val data = getAnnualData(aYear)
         val salaryInfoList = data.salaryInfoList
         var value = 0
@@ -102,6 +323,227 @@ class AnnualAnalyzePresenter(var mActivity: IAnnualAnalyze) : IAnnualAnalyzePres
             // 基本給
             value += salaryInfo.baseIncome
         }
-        return AnnualDataRow(R.string.annual_analyze_row_base_income, value.toString(), R.string.annual_analyze_row_unit_money, true)
+        return value
     }
+
+    /**
+     * 残業代の合計を算出する
+     */
+    private fun getSumOverTimeIncome(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 残業代
+            value += salaryInfo.overtimeIncome
+        }
+        return value
+    }
+
+    /**
+     * その他収入の合計を算出する
+     */
+    private fun getSumOtherIncome(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 残業代
+            value += salaryInfo.otherIncome
+        }
+        return value
+    }
+
+    /**
+     * ボーナスの合計を算出する
+     */
+    private fun getSumBonus(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(bonusInfo in bonusInfoList) {
+            // 基本支給
+            value += bonusInfo.baseIncome
+            // その他
+            value += bonusInfo.otherIncome
+        }
+        return value
+    }
+
+    /**
+     * 控除額の合計を算出する
+     */
+    private fun getSumDeduction(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 健康保険
+            value += salaryInfo.healthInsuranceFee
+            // 介護保険
+            value += salaryInfo.longTermCareInsuranceFee
+            // 年金
+            value += salaryInfo.pensionFee
+            // 雇用保険
+            value += salaryInfo.employmentInsuranceFee
+            // 所得税
+            value += salaryInfo.incomeTax
+            // 住民税
+            value += salaryInfo.residentTax
+            // その他控除
+            value += salaryInfo.otherDeduction
+        }
+        for(bonusInfo in bonusInfoList) {
+            // 健康保険
+            value += bonusInfo.healthInsuranceFee
+            // 介護保険
+            value += bonusInfo.longTermCareInsuranceFee
+            // 年金
+            value += bonusInfo.pensionFee
+            // 雇用保険
+            value += bonusInfo.employmentInsuranceFee
+            // 所得税
+            value += bonusInfo.incomeTax
+            // その他控除
+            value += bonusInfo.otherDeduction
+        }
+        return value
+    }
+
+    /**
+     * 健康保険の合計を算出する
+     */
+    private fun getSumHealthInsuranceFee(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 健康保険
+            value += salaryInfo.healthInsuranceFee
+        }
+        for(bonusInfo in bonusInfoList) {
+            // 健康保険
+            value += bonusInfo.healthInsuranceFee
+        }
+        return value
+    }
+
+    /**
+     * 介護保険の合計を算出する
+     */
+    private fun getSumLongTermCareInsuranceFee(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 介護保険
+            value += salaryInfo.longTermCareInsuranceFee
+        }
+        for(bonusInfo in bonusInfoList) {
+            // 介護保険
+            value += bonusInfo.longTermCareInsuranceFee
+        }
+        return value
+    }
+
+    /**
+     * 年金保険の合計を算出する
+     */
+    private fun getSumPensionFee(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 年金
+            value += salaryInfo.pensionFee
+        }
+        for(bonusInfo in bonusInfoList) {
+            // 年金
+            value += bonusInfo.pensionFee
+        }
+        return value
+    }
+
+    /**
+     * 雇用保険の合計を算出する
+     */
+    private fun getSumEmploymentInsuranceFee(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 雇用保険
+            value += salaryInfo.employmentInsuranceFee
+        }
+        for(bonusInfo in bonusInfoList) {
+            // 雇用保険
+            value += bonusInfo.employmentInsuranceFee
+        }
+        return value
+    }
+
+    /**
+     * 所得税の合計を算出する
+     */
+    private fun getSumIncomeTax(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 所得税
+            value += salaryInfo.incomeTax
+        }
+        for(bonusInfo in bonusInfoList) {
+            // 所得税
+            value += bonusInfo.incomeTax
+        }
+        return value
+    }
+
+    /**
+     * 住民税の合計を算出する
+     */
+    private fun getSumResidentTax(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // 住民税
+            value += salaryInfo.residentTax
+        }
+        return value
+    }
+
+    /**
+     * その他の控除の合計を算出する
+     */
+    private fun getSumOtherDeduction(aYear: Int): Int {
+        val data = getAnnualData(aYear)
+        val salaryInfoList = data.salaryInfoList
+        val bonusInfoList = data.bonusInfoList
+        var value = 0
+        for(salaryInfo in salaryInfoList){
+            // その他
+            value += salaryInfo.otherDeduction
+        }
+        for(bonusInfo in bonusInfoList) {
+            // その他
+            value += bonusInfo.otherDeduction
+        }
+        return value
+    }
+
+    /**
+     * 手取り額を取得する
+     */
+    private fun getSumAfterTaxIncome(aYear: Int): Int {
+        return getSumIncome(aYear) - getSumDeduction(aYear)
+    }
+
 }
